069030a752948e9e13059cf3f69745aeb6ac4b8e
averyli290 <averyli408@gmail.com>
2020-02-29 14:26:05 -0800

    first commit

---
+++ src/GameScreen.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class GameScreen {
+    public static void main(String[] args) {
+        System.out.println("Screen Initialized");
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,16 @@
+import org.javatuples.Pair;
+
+public class Player {
+
+    // Instance variables
+    String name;
+    Pair <Integer, Integer> position;
+    
+    // Constructor Declaration of Class
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public 
+
+}
